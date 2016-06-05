ab15ba947b72d3de9f5862008776188a099b600b
SEILA <seila.tupone@gmail.com>
2016-06-05 00:24:18 -0400

    Method sum_price

--- receipt.rb
+++ receipt.rb
@@ -5,6 +5,7 @@ class Receipt
     attr_reader :total_taxes, :total_price
 
   def initialize
+    @total = []
     @total_price = []
     @total_taxes = []
     @total_price_no_taxes = []
@@ -14,57 +15,53 @@ class Receipt
 # Calculate item price taxes included
   def sales_taxes(item)
     if (item.name == 'Book' || item.name == 'Food' || item.name == 'Medical')
-      total = item.price
-      puts "1 #{item.name} at #{total}"
+      @total = item.price
+      puts "1 #{item.name} at #{@total}"
     else
-      total = item.price * 1.10
-      puts "1 #{item.name} at #{total}"
+      @total = item.price * 1.10
+      puts "1 #{item.name} at #{@total}"
     end
 
-    @total_price << total
-    @total_price.each do |x|
-      @sum_total_price += x
-      puts "#{@sum_total_price}"
-    end
     add(item)
   end
 
 # Calculate imported item's price
   def import_taxes(item)
     if ((item.name == 'Book' || item.name == 'Food' || item.name == 'Medical') && item.import_taxes == false)
-      total = item.price
-      puts "1 #{item.name} at #{total}"
+      @total = item.price
+      puts "1 #{item.name} at #{@total}"
     elsif ((item.name == 'Book' || item.name == 'Food' || item.name == 'Medical') && item.import_taxes == true)
-      total = item.price * 1.05
-      puts "1 imported #{item.name} at #{total}"
+      @total = item.price * 1.05
+      puts "1 imported #{item.name} at #{@total}"
     else
-      total = item.price * 1.15
-      puts "1 imported #{item.name} at #{total}"
+      @total = item.price * 1.15
+      puts "1 imported #{item.name} at #{@total}"
     end
 
-    @total_price << total
-    @total_price.each do |x|
-      @sum_total_price += x
 
-    end
 # Call method add
     add(item)
-
   end
 
 # Add items into an array
   def add(item)
     if item.is_a?(Item)
       @total_price_no_taxes << item.price
+      @total_price << @total
     else
       raise "Error in method add"
     end
   end
 
+
 # Sum of all products in the array
   def sum_price
     @total_price_no_taxes.each do |x|
       @sum += x
+    end
+
+    @total_price.each do |x|
+      @sum_total_price += x
     end
   end
 
@@ -81,7 +78,7 @@ class Receipt
 end
 
 # Create instances of Item class
-book = Item.new("Book", 10, true, true)
+book = Item.new("Book", 10, false, false)
 food = Item.new("Food", 30, false, true)
 cd = Item.new("Cd", 20, true, true)
 # Create instance of Receipt class
@@ -91,10 +88,11 @@ receipt = Receipt.new
 receipt.sales_taxes(book)
 receipt.import_taxes(book)
 
-#receipt.sales_taxes(food)
+receipt.sales_taxes(food)
 receipt.import_taxes(food)
 
-#receipt.sales_taxes(cd)
+receipt.sales_taxes(cd)
 receipt.import_taxes(cd)
 receipt.sum_price
+
 receipt.cart
